767f26ae9a73996318174373232753dbd90504e9
Boris <watasinonamaewa@gmail.com>
2022-07-01 21:15:13 +0400

    123

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
-import { Form, Button } from 'react-bootstrap';
+import {
+  Form, Button, Row, Col,
+} from 'react-bootstrap';
 
 function Login() {
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>name</Form.Label>
-        <Form.Control type="name" placeholder="Enter your nickname" />
-      </Form.Group>
+    <Form className="w-50 border border-light">
+      <Row>
+        <Col />
+        <Col>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>name</Form.Label>
+            <Form.Control type="name" placeholder="Enter your nickname" />
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Enter your password" />
-      </Form.Group>
-      <div className="d-grid gap-2">
-        <Button variant="outline-primary" type="submit">
-          Log in
-        </Button>
-      </div>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Enter your password" />
+          </Form.Group>
+          <div className="d-grid gap-2">
+            <Button variant="outline-primary" type="submit">
+              Log in
+            </Button>
+          </div>
+        </Col>
+      </Row>
     </Form>
   );
 }

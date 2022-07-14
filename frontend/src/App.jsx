e72fcc7a3b5661fa2828f659c62ccc1132fbaaeb
Boris <watasinonamaewa@gmail.com>
2022-07-14 16:13:39 +0400

    step 3 in progress

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,39 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+/* eslint-disable react/jsx-no-constructed-context-values, react/prop-types */
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router, Routes, Route,
+} from 'react-router-dom';
 import './App.css';
-import Forbidden from './components/404.jsx';
+import Forbidden from './components/pages/404.jsx';
 import LoginPage from './components/pages/LoginPage.jsx';
+import AuthContext from './context/AuthContext.jsx';
+
+function AuthProvider({ children }) {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const logIn = () => setLoggedIn(true);
+  const logOut = () => {
+    localStorage.removeItem('userId');
+    setLoggedIn(false);
+  };
+  return (
+    <AuthContext.Provider value={{ loggedIn, logIn, logOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/login" element={<LoginPage />} />
-        <Route exact path="/" element={<div />} />
-        <Route path="*" element={<Forbidden />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route exact path="/login" element={<LoginPage />} />
+          <Route exact path="/" element={<div />} />
+          <Route path="*" element={<Forbidden />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,42 +1,58 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import {
   Form, Button,
 } from 'react-bootstrap';
+import { useNavigate } from 'react-router';
 import { useFormik } from 'formik';
 import { string, object } from 'yup';
-import cn from 'classnames';
+import axios from 'axios';
+// import authContext from '../context/AuthContext.jsx';
+// import cn from 'classnames';
 
 function LoginForm() {
+  const inputRef = useRef(null);
+  const navigate = useNavigate();
+  // const auth = useContext(authContext);
   const formik = useFormik({
     initialValues: {
-      name: '',
+      username: '',
       password: '',
     },
     validationSchema: object().shape({
-      name: string().required('Required').min(4, 'Must be at least 4 symbols'),
-      password: string().required('Required').min(5, 'Must be at least 5 symbols'),
+      username: string().required('Name is a required field').min(4, 'Must be at least 4 symbols'),
+      password: string().required('Password is a required field').min(5, 'Must be at least 5 symbols'),
     }),
     validateOnBlur: false,
     validateOnChange: false,
     onSubmit: (values) => {
-      console.log(formik);
-      alert(JSON.stringify(values));
+      axios.post('/api/v1/login', values)
+        .then(({ data }) => {
+          localStorage.setItem('userId', JSON.stringify(data));
+          navigate('/');
+        })
+        .catch((err) => {
+          if (err.isAxiosError && err.response.status === 401) {
+            formik.errors.username = 'User does not exist';
+          }
+          throw err;
+        });
     },
   });
   return (
     <Form className="col-12 col-md-6 mt-3 mt-mb-0" noValidate onSubmit={formik.handleSubmit}>
       <h1 className="text-center mb-4">Войти</h1>
-      <Form.Group className="form-floating mb-3" controlId="name">
+      <Form.Group className="form-floating mb-3" controlId="username">
         <Form.Control
           type="name"
           placeholder="Ваш ник"
           onChange={formik.handleChange}
-          value={formik.values.name}
-          isValid={formik.touched.name && !formik.errors.name}
+          value={formik.values.username}
+          isInvalid={!!formik.errors.username}
+          ref={inputRef}
         />
         <Form.Label className="form-label">Ваш ник</Form.Label>
-        <Form.Control.Feedback>
-          {formik.errors.name}
+        <Form.Control.Feedback type="invalid">
+          {formik.errors.username}
         </Form.Control.Feedback>
       </Form.Group>
 
@@ -46,10 +62,10 @@ function LoginForm() {
           placeholder="Пароль"
           onChange={formik.handleChange}
           value={formik.values.password}
-          isValid={formik.touched.password && !formik.errors.password}
+          isInvalid={!!formik.errors.password}
         />
         <Form.Label className="form-label">Пароль</Form.Label>
-        <Form.Control.Feedback>
+        <Form.Control.Feedback type="invalid">
           {formik.errors.password}
         </Form.Control.Feedback>
       </Form.Group>
@@ -63,17 +79,3 @@ function LoginForm() {
 }
 
 export default LoginForm;
-/* <form className="col-12 col-md-6 mt-3 mt-mb-0">
-      <h1 className="text-center mb-4">Войти</h1>
-      <div className="form-floating mb-3">
-        <input name="username" required="" placeholder="Ваш ник"
-        id="username" className="form-control" value="" />
-        <label className="form-label" htmlFor="username">Ваш ник</label>
-      </div>
-      <div className="form-floating mb-4">
-        <input name="password" required="" placeholder="Пароль"
-        type="password" id="password" className="form-control" value="" />
-        <label className="form-label" htmlFor="password">Пароль</label>
-      </div>
-      <button type="submit" className="w-100 mb-3 btn btn-outline-primary">Войти</button>
-    </form> */

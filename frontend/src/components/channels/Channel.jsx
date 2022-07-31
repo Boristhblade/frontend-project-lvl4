8b0369b9546f7bec6cf350309654b2e94a05e5a2
Boris <watasinonamaewa@gmail.com>
2022-08-01 01:55:03 +0400

    fix Channel.jsx

--- frontend/src/components/channels/Channel.jsx
+++ frontend/src/components/channels/Channel.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Dropdown, ButtonGroup, Button } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
+import cn from 'classnames';
 import { setChannel } from '../../slices/currentChannelSlice.js';
 import useModal from '../../hooks/useModal.jsx';
 
@@ -10,21 +11,23 @@ export default function Channel({ name, id, removable }) {
   const { t } = useTranslation();
   const openModal = useModal();
   const dispatch = useDispatch();
+  const activeChannelId = useSelector((state) => state.currentChannel.currentChannel);
+  const variant = cn({ light: id !== activeChannelId, secondary: id === activeChannelId});
   const button = (
-    <Button variant="light" className="w-100 rounded-0 text-start btn" onClick={() => dispatch(setChannel({ id }))}>
+    <Button variant={variant} className="w-100 rounded-0 text-start btn" onClick={() => dispatch(setChannel({ id }))}>
       <span className="me-1">#</span>
       {name}
     </Button>
   );
   const dropdown = (
     <Dropdown as={ButtonGroup} className="w-100">
-      <Button variant="light" className="rounded-0 text-start w-100 text-truncate" onClick={() => dispatch(setChannel({ id }))}>
+      <Button variant={variant}  className="rounded-0 text-start w-100 text-truncate" onClick={() => dispatch(setChannel({ id }))}>
         #
         {' '}
         {name}
       </Button>
 
-      <Dropdown.Toggle split variant="light" className="flex-grow-0 text-end">
+      <Dropdown.Toggle split variant={variant}  className="flex-grow-0 text-end">
         <span className="visually-hidden">{t('channel.manage')}</span>
       </Dropdown.Toggle>
 

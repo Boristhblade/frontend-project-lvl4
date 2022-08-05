590fe3d77bc678c249996bb882f5f97ce1f505da
Boris <watasinonamaewa@gmail.com>
2022-08-05 11:02:19 +0400

    add autoscroll to last message

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Message from './Message.jsx';
 import MessageForm from './MessageForm.jsx';
 import MessagesHeader from './MessagesHeader.jsx';
 
 export default function Messages() {
+  const scrollRef = useRef(null);
   const messages = useSelector((state) => state.messages);
   const activeChannelId = useSelector((state) => state.currentChannel.currentChannel);
   const filteredMessages = messages.ids
     .filter((id) => messages.entities[id].channelId === activeChannelId);
   const channels = useSelector((state) => state.channels);
   const currentChannelName = channels.entities[activeChannelId]?.name;
+  useEffect(() => {
+    scrollRef.current.scrollIntoView();
+  })
 
   return (
     <div className="col p-0 h-100">
@@ -25,6 +29,7 @@ export default function Messages() {
                 username={messages.entities[id].username}
               />
             ))}
+            <div ref={scrollRef} />
         </div>
         <MessageForm />
       </div>
